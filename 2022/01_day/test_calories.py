210f673ff668b4f8be449781ecc756b770a8d626
ljmesi <37740329+ljmesi@users.noreply.github.com>
2022-12-05 21:05:48 +0100

    Update few sanity checks

--- 2022/01_day/test_calories.py
+++ 2022/01_day/test_calories.py
@@ -4,16 +4,16 @@
 from subprocess import getstatusoutput
 
 prg = "./calories.py"
-ex = "elfs_and_calories.json"
-test1 = "test1.json"
-test2 = "test2.json"
+ex = "aoc_input.txt"
+test1 = "test1.txt"
+test2 = "test2.txt"
 
 # --------------------------------------------------
 def test_ex():
     """Test on more than one file"""
     rv, out = getstatusoutput(f"{prg} --file {ex}")
     assert rv == 0
-    assert out.rstrip() == "Elf4 has most calories.\nHe has 24000 calories."
+    assert out.rstrip() == "Elf113 has most calories.\nHe has 71506 calories."
 
 
 # --------------------------------------------------
@@ -21,12 +21,4 @@ def test_1():
     """Test on more than one file"""
     rv, out = getstatusoutput(f"{prg} --file {test1}")
     assert rv == 0
-    assert out.rstrip() == "Elf1 has most calories.\nHe has 6000 calories."
-
-
-# --------------------------------------------------
-def test_2():
-    """Test on more than one file"""
-    rv, out = getstatusoutput(f"{prg} --file {test2}")
-    assert rv == 0
-    assert out.rstrip() == "Elf3 has most calories.\nHe has 6000 calories."
+    assert out.rstrip() == "Elf3 has most calories.\nHe has 6 calories."
