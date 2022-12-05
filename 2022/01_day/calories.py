4b65a6dcd7caaf6621093ea0b4bb6f949f3ba666
ljmesi <37740329+ljmesi@users.noreply.github.com>
2022-12-05 21:07:30 +0100

    Update solution to read lists

--- 2022/01_day/calories.py
+++ 2022/01_day/calories.py
@@ -7,8 +7,6 @@ __version__ = "0.1.0"
 
 import argparse
 import dataclasses
-import json
-import logging
 from pathlib import Path
 from pydantic.dataclasses import dataclass
 
@@ -49,22 +47,37 @@ def get_args():
     return parser.parse_args()
 
 
-def main() -> None:
+def main():
     """Main function"""
     # Parse CLI arguments
     args: argparse.Namespace = get_args()
-    with open(args.file, encoding="utf8") as fh:
-        data = json.load(fh)
-        # Unpacking items to keyword arguments (kwargs) with **
-        elfs_with_foods: list[ElfFoods] = [ElfFoods(**item) for item in data]
-        elfs_with_foods_sorted = sorted(
-            elfs_with_foods, key=lambda x: x.total, reverse=True
-        )
-        elf_with_most_calories = elfs_with_foods_sorted[0]
-        print(
-            f"{elf_with_most_calories.name} has most calories.\n"
-            f"He has {elf_with_most_calories.total} calories."
-        )
+
+    counter: int = 1
+    current_elf_calories: list = []
+    all_elf_calories: list = []
+
+    with open(args.file, encoding="utf8") as filehandle:
+        for element in [line.rstrip() for line in filehandle]:
+            if element:
+                current_elf_calories.append(int(element))
+            else:
+                all_elf_calories.append(
+                    {"name": f"Elf{counter}", "calories": current_elf_calories.copy()}
+                )
+                current_elf_calories.clear()
+                counter += 1
+
+    # Sort the elfs by calories
+    elfs_with_foods: list[ElfFoods] = [ElfFoods(**elf) for elf in all_elf_calories]
+    elfs_with_foods_sorted = sorted(
+        elfs_with_foods, key=lambda x: x.total, reverse=True
+    )
+    # Pick the one with the most
+    elf_with_most_calories: ElfFoods = elfs_with_foods_sorted[0]
+    print(
+        f"{elf_with_most_calories.name} has most calories.\n"
+        f"He has {elf_with_most_calories.total} calories."
+    )
 
 
 if __name__ == "__main__":
